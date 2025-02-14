979c9f7e8bcf02b09371acca5287f7c3972c4d53
hamhyeongju <gudwn357@gmail.com>
2025-02-14 11:16:26 +0900

    feat: Add User

---
+++ src/main/java/prography/table_tennis/domain/User.java
@@ -0,0 +1,19 @@
+package prography.table_tennis.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Integer id;
+    private Integer fakerId;
+    private String name;
+    private String email;
+    private String status;
+}
