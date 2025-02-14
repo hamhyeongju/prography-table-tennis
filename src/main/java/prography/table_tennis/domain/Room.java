6c1f1671fc5dba15219835e82eafcc795cb8122f
hamhyeongju <gudwn357@gmail.com>
2025-02-14 11:20:33 +0900

    feat: Add Room

---
+++ src/main/java/prography/table_tennis/domain/Room.java
@@ -0,0 +1,19 @@
+package prography.table_tennis.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "room_id")
+    private Integer id;
+    private String title;
+    private Integer host; // userId
+    private String roomType; // SINGLE, DOUBLE
+    private String status; // WAIT, PROGRESS, FINISH
+}
