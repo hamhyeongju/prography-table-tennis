6d4ce37913181726d308012a74f0611c459c8525
hamhyeongju <gudwn357@gmail.com>
2025-02-14 10:35:28 +0900

    first commit

---
+++ src/main/java/prography/table_tennis/TableTennisApplication.java
@@ -0,0 +1,13 @@
+package prography.table_tennis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableTennisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TableTennisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/prography/table_tennis/TableTennisApplicationTests.java
@@ -0,0 +1,13 @@
+package prography.table_tennis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableTennisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
