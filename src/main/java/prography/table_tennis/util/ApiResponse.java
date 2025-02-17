03b08ba9f606628f24f2b3abfbc4d262fd6885e3
hamhyeongju <gudwn357@gmail.com>
2025-02-17 10:28:59 +0900

    feat: Add ApiResponse
    성공 시 응답만 추가

---
+++ src/main/java/prography/table_tennis/util/ApiResponse.java
@@ -0,0 +1,23 @@
+package prography.table_tennis.util;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+
+import static prography.table_tennis.util.ApiResponseStatus.SUCCESS;
+
+@Getter
+public class ApiResponse<T> {
+    private Integer code;
+    private String message;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private T result;
+
+    public ApiResponse() {
+        success();
+    }
+
+    private void success() {
+        this.code = SUCCESS.getCode();
+        this.message = SUCCESS.getMessage();
+    }
+}
