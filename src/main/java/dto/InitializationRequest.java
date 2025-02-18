83d1a452c7992b6b2da4a10a3a8b467b088fbd37
hamhyeongju <gudwn357@gmail.com>
2025-02-18 10:29:07 +0900

    feat: Add InitializationRequest

---
+++ src/main/java/dto/InitializationRequest.java
@@ -0,0 +1,17 @@
+package dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class InitializationRequest {
+
+    private int seed;
+    private int quantity;
+}
