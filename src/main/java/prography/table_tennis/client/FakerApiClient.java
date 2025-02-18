88c5b7486e4816353a724ccd9473b6676ab06a36
hamhyeongju <gudwn357@gmail.com>
2025-02-18 10:12:11 +0900

    feat: Add FakerApiClient

---
+++ src/main/java/prography/table_tennis/client/FakerApiClient.java
@@ -0,0 +1,18 @@
+package prography.table_tennis.client;
+
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import prography.table_tennis.client.dto.FakeUserResponse;
+
+@FeignClient(name = "fakerApiClient", url = "https://fakerapi.it/api/v1/users")
+public interface FakerApiClient {
+
+    @GetMapping
+    FakeUserResponse getUsers(
+            @RequestParam("_seed") int seed,
+            @RequestParam("_quantity") int quantity,
+            @RequestParam("_locale") String locale
+    );
+}
