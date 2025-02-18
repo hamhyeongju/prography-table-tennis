137f4ca43dd45549ebe6595031e7b740bfd1df5f
hamhyeongju <gudwn357@gmail.com>
2025-02-18 10:29:00 +0900

    feat: Add InitializationController

---
+++ src/main/java/prography/table_tennis/controller/InitializationController.java
@@ -0,0 +1,24 @@
+package prography.table_tennis.controller;
+
+import dto.InitializationRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import prography.table_tennis.service.InitializationService;
+import prography.table_tennis.util.ApiResponse;
+
+@RestController
+@RequiredArgsConstructor
+public class InitializationController {
+
+    private final InitializationService initializationService;
+
+    @PostMapping("/init")
+    public ApiResponse<Object> init(@RequestBody InitializationRequest request) {
+
+        initializationService.init(request);
+
+        return new ApiResponse<>();
+    }
+}
