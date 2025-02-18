90d2146bb287c36afd0446a4107c5da330f08bde
hamhyeongju <gudwn357@gmail.com>
2025-02-18 10:12:31 +0900

    feat: Add FakeUser

---
+++ src/main/java/prography/table_tennis/client/dto/FakeUser.java
@@ -0,0 +1,13 @@
+package prography.table_tennis.client.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FakeUser {
+    private Integer id;
+    private String username;
+    private String email;
+    private String phone;
+}
