2d688c411caa6d37338ecdbe3fb4c9e212ca0922
hamhyeongju <gudwn357@gmail.com>
2025-02-19 14:24:17 +0900

    feat: add UserService for get users

---
+++ src/main/java/prography/table_tennis/service/UserService.java
@@ -0,0 +1,33 @@
+package prography.table_tennis.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import prography.table_tennis.domain.User;
+import prography.table_tennis.dto.GetUsersResponse;
+import prography.table_tennis.dto.UserResponse;
+import prography.table_tennis.repository.UserRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public GetUsersResponse getUsers(Pageable pageable) {
+
+        Page<User> findUsers = userRepository.findAll(pageable);
+
+        List<UserResponse> userResponses =
+                findUsers.getContent().
+                stream().map(UserResponse::new).toList();
+
+        return new GetUsersResponse(
+                (int) findUsers.getTotalElements(),
+                findUsers.getTotalPages(),
+                userResponses);
+    }
+}
