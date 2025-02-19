f66a9737ac2133fb3a8161da2c6a8b1a29a98c3a
hamhyeongju <gudwn357@gmail.com>
2025-02-19 16:03:18 +0900

    feat: add ControllerAdvice

---
+++ src/main/java/prography/table_tennis/controller/ControllerAdvice.java
@@ -0,0 +1,26 @@
+package prography.table_tennis.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import prography.table_tennis.exception.DomainException;
+import prography.table_tennis.util.ApiResponse;
+
+import static prography.table_tennis.util.ApiResponseStatus.*;
+
+@RestControllerAdvice
+public class ControllerAdvice {
+
+    @ExceptionHandler(DomainException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiResponse<Object> handleDomainException(DomainException e) {
+        return new ApiResponse<>(WRONG_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ApiResponse<Object> handleException(Exception e) {
+        return new ApiResponse<>(SERVER_ERROR);
+    }
+}
