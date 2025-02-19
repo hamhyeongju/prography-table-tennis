ba7b4b6ffc8979d6ec28a405a2be20090a7746cd
hamhyeongju <gudwn357@gmail.com>
2025-02-19 10:37:12 +0900

    feat: Add UserBulkInsertDto

---
+++ src/main/java/dto/UserBulkInsertDto.java
@@ -0,0 +1,39 @@
+package dto;
+
+import lombok.Getter;
+import prography.table_tennis.domain.UserStatus;
+
+@Getter
+public class UserBulkInsertDto {
+
+    private Integer fakerId;
+    private String name;
+    private String email;
+    private String status;
+
+    public static UserBulkInsertDto createActiveUserDto (Integer fakerId, String name, String email) {
+        UserBulkInsertDto dto = createUserBulkInsert(fakerId, name, email);
+        dto.status = UserStatus.ACTIVE.toString();
+        return dto;
+    }
+    public static UserBulkInsertDto createWaitUserDto (Integer fakerId, String name, String email) {
+        UserBulkInsertDto dto = createUserBulkInsert(fakerId, name, email);
+        dto.status = UserStatus.WAIT.toString();
+        return dto;
+    }
+    public static UserBulkInsertDto createNonActiveUserDto (Integer fakerId, String name, String email) {
+        UserBulkInsertDto dto = createUserBulkInsert(fakerId, name, email);
+        dto.status = UserStatus.NON_ACTIVE.toString();
+        return dto;
+    }
+
+
+    private static UserBulkInsertDto createUserBulkInsert(Integer fakerId, String name, String email) {
+        UserBulkInsertDto dto = new UserBulkInsertDto();
+        dto.fakerId = fakerId;
+        dto.name = name;
+        dto.email = email;
+        return dto;
+    }
+
+}
