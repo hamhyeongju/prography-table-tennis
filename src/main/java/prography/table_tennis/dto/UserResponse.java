5136733fd027796d5869fd712d284b36b2761abf
hamhyeongju <gudwn357@gmail.com>
2025-02-19 14:23:53 +0900

    feat: add constructor using user

--- src/main/java/prography/table_tennis/dto/UserResponse.java
+++ src/main/java/prography/table_tennis/dto/UserResponse.java
@@ -1,12 +1,14 @@
 package prography.table_tennis.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import prography.table_tennis.domain.User;
 
 import java.time.LocalDateTime;
 
 @Getter
-@AllArgsConstructor
 public class UserResponse {
 
     private int id;
@@ -16,5 +18,15 @@ public class UserResponse {
     private String status;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
+    public UserResponse(User user) {
+        this.id = user.getId();
+        this.fakerId = user.getFakerId();
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.status = user.getStatus();
+        this.createdAt = user.getCreatedAt();
+        this.updatedAt = user.getUpdatedAt();
+    }
 }
 
