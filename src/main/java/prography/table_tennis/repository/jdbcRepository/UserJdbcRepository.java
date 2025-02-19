19332dbfc7e7e0b95e996d9a96dbd9460d13554e
hamhyeongju <gudwn357@gmail.com>
2025-02-19 10:37:03 +0900

    feat: Add UserJdbcRepository
    bulk insert 쿼리 생성하는 리포지토리

---
+++ src/main/java/prography/table_tennis/repository/jdbcRepository/UserJdbcRepository.java
@@ -0,0 +1,30 @@
+package prography.table_tennis.repository.jdbcRepository;
+
+import dto.UserBulkInsertDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class UserJdbcRepository {
+
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+    public void bulkInsertUser(List<UserBulkInsertDto> dtos) {
+        String sql = "INSERT INTO users (faker_id, name, email, status) " +
+                "VALUES (:fakerId, :name, :email, :status)";
+
+        SqlParameterSource[] sqlParameterSource =
+                dtos.stream().map(
+                                BeanPropertySqlParameterSource::new).
+                        toArray(SqlParameterSource[]::new);
+
+        jdbcTemplate.batchUpdate(sql, sqlParameterSource);
+    }
+
+}
