bc81c51d44e52ea4cdde4e22dcd9588651943a87
hamhyeongju <gudwn357@gmail.com>
2025-02-19 14:41:03 +0900

    feat: specify name attribute in @RequestParam

--- src/main/java/prography/table_tennis/controller/UserController.java
+++ src/main/java/prography/table_tennis/controller/UserController.java
@@ -1,10 +1,12 @@
 package prography.table_tennis.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import prography.table_tennis.dto.GetUsersResponse;
+import prography.table_tennis.service.UserService;
 import prography.table_tennis.util.ApiResponse;
 
 @RestController
@@ -13,13 +15,13 @@ public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/users")
+    @GetMapping("/user")
     public ApiResponse<GetUsersResponse> getUsers(
-            @RequestParam int size,
-            @RequestParam int page
+            @RequestParam("size") int size,
+            @RequestParam("page") int page
     ) {
 
-        GetUsersResponse response = userService.getUsers(size, page);
+        GetUsersResponse response = userService.getUsers(PageRequest.of(page, size));
 
         return new ApiResponse<>(response);
     }
