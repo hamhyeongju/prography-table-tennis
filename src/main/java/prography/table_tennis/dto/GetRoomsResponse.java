4749d636252c003c049dc7b4e8efa148b9f9ff76
hamhyeongju <gudwn357@gmail.com>
2025-02-20 13:18:13 +0900

    feat: add GetRoomsResponse

---
+++ src/main/java/prography/table_tennis/dto/GetRoomsResponse.java
@@ -0,0 +1,16 @@
+package prography.table_tennis.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class GetRoomsResponse {
+
+    private int totalElements;
+    private int totalPages;
+    private List<RoomResponse> roomList;
+
+}
