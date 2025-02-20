b641fa3fdb7801896d527f433b3b70e5b0afdd7c
hamhyeongju <gudwn357@gmail.com>
2025-02-20 10:41:57 +0900

    feat: add enum constant

--- src/main/java/prography/table_tennis/util/ApiResponseStatus.java
+++ src/main/java/prography/table_tennis/util/ApiResponseStatus.java
@@ -7,7 +7,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ApiResponseStatus {
 
-    SUCCESS(200, "API 요청이 성공했습니다.");
+    SUCCESS(200, "API 요청이 성공했습니다."),
+    WRONG_REQUEST(201, "불가능한 요청입니다."),
+    SERVER_ERROR(500, "에러가 발생했습니다.");
 
     private final int code;
     private final String message;
